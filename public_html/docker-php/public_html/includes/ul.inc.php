5c443115113e19c0250f17655d4bde2d87240733
Legrand Thomas <pro.legrand.thomas@gmail.com>
2022-04-28 09:34:18 +0200

    1

---
+++ public_html/docker-php/public_html/includes/ul.inc.php
@@ -0,0 +1,47 @@
+<!-- <nav>
+    <button type="button" class="btn btn-outline-secondary">Home
+        </button>
+        <ul>
+            <li>Debogage</li>
+            <li>Concaténation</li>
+            <li>Boucle</li>
+            <li>Fonction</li>
+            <li>Supprimer</li>
+        </ul>
+</nav> -->
+
+<div class="container">
+    <div class="d-flex p-2 bd-highlight w-25 p-3" >
+
+        <nav>
+            <button type="button" class="btn btn-outline-secondary w-100 p-2" >Home </button>
+                <ul class="list-group">
+                    <li class="list-group-item active" aria-current="true">An active item</li>
+                    <button type="button" class="list-group-item list-group-item-action">A second item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A third button item</button>
+                    <button type="button" class="list-group-item list-group-item-action">A fourth button item</button>
+                    <li class="list-group-item">A second item</li>
+                    <li class="list-group-item">A third item</li>
+                    <li class="list-group-item">A fourth item</li>
+                    <li class="list-group-item">And a fifth one</li>
+                </ul>
+        </nav>
+
+    </div>
+</div>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
--- public_html/docker-php/public_html/index.php
+++ public_html/docker-php/public_html/index.php
@@ -1,3 +1,40 @@
-<?php
+<!DOCTYPE html>
+<html lang="fr">
 
-phpinfo();
+    <?php  include 'includes/head.inc.html';  ?>
+<body>
+     <?php include 'includes/header.inc.html';  ?>
+     
+
+     <div class="container">
+        <div class="row">
+            <div class="col-3">
+                <nav>
+                    <div class="d-grid gap-2">
+                    
+                            <button type="button" class="btn btn-outline-secondary btn-block">Home</button>
+                    
+                    </div>
+                </nav>
+            </div>
+
+            <div class="col-9">         
+                <section>
+                    <button type="button" class="btn btn-primary">Ajouter des données</button>
+                    
+                    <?php  include 'includes/form.inc.html'; ?>
+                </section>
+            </div>
+        </div>   
+    </div>
+    
+       
+         
+         <!-- include 'includes/ul.inc.php'; -->
+         
+         
+         
+    <?php include 'includes/footer.inc.html';  ?>
+
+</body>
+</html>
